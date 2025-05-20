d7f6531e060790650a5566e6e816cf547f12d921
Jhommuel Pastoral <pastoraljhommuel@gmail.com>
2025-05-20 10:07:21 +0800

    change server:

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://linkup-ub4p.onrender.com",
     credentials: true
   }
 });
@@ -62,7 +62,7 @@ io.on("connection", (socket) => {
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://linkup-ub4p.onrender.com",
   credentials: true
 }));
 
